b2e66be8fe77bb101ecac04b11dc8b877d71c7f9
Pradeep Kundar <pradeepkundar04@gmail.com>
2022-11-04 16:28:33 +0530

    latest-updates

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
-import Header from "./components/header/Header";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
+import Products from "./components/Products/Products";
 import Slider from "./components/Slider/Slider";
+import Testimonials from "./components/Testimonials/Testimonials";
+import Virtual from "./components/Virtual/Virtual";
 
 function App() {
   return (
@@ -9,6 +13,10 @@ function App() {
       <Header />
       <Hero />
       <Slider />
+      <Virtual />
+      <Products />
+      <Testimonials />
+      <Footer />
     </div>
   );
 }
